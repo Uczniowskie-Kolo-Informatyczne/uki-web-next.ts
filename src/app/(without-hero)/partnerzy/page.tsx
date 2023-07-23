e20df8b1dfb6a330a81f309a2b316e49a6a2d252
Wojtazzzz <marcin.witas72@gmail.com>
2023-07-23 16:09:56 +0200

    fix

--- src/app/(without-hero)/partnerzy/page.tsx
+++ src/app/(without-hero)/partnerzy/page.tsx
@@ -37,7 +37,7 @@ export default async function PartnersPage() {
 												src={image}
 												alt=""
 												fill
-												className="object-cover"
+												className="aspect-square"
 											/>
 										</div>
 
